e1b3aafb3174ee546d4bb701ce08daa41d72f959
chad-ermacora <chad.e@linux.com>
2018-09-12 07:32:52 -0700

    Another Commit

--- KootNetSensors_GUI.py
+++ KootNetSensors_GUI.py
@@ -18,6 +18,12 @@
 """
 import os
 import sys
+# DEBUG  - 	Detailed information, typically of interest only when diagnosing problems.
+# INFO	-  Confirmation that things are working as expected.
+# WARNING	-  An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+# ERROR	-  Due to a more serious problem, the software has not been able to perform some function.
+# CRITICAL	-  A serious error, indicating that the program itself may be unable to continue running.
+import logging
 import Sensor_commands
 import Sensor_app_imports
 import Sensor_graphs
