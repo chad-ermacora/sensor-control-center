0a74ced9cc566db25259e7575ef0b92676553049
chad-ermacora <chad.e@linux.com>
2018-10-12 20:56:18 -0700

    Refactoring

--- app_reports.py
+++ app_reports.py
@@ -38,9 +38,9 @@ logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
 app_location_directory = str(os.path.dirname(sys.argv[0])) + "/"
-html_template_details1 = "additional_files/html_template_system1.html"
-html_template_details2 = "additional_files/html_template_system2.html"
-html_template_details3 = "additional_files/html_template_system3.html"
+html_template_system1 = "additional_files/html_template_system1.html"
+html_template_system2 = "additional_files/html_template_system2.html"
+html_template_system3 = "additional_files/html_template_system3.html"
 html_template_config1 = "additional_files/html_template_config1.html"
 html_template_config2 = "additional_files/html_template_config2.html"
 html_template_config3 = "additional_files/html_template_config3.html"
@@ -49,15 +49,15 @@ html_template_config3 = "additional_files/html_template_config3.html"
 def html_system_codes():
     logger.debug("Getting Sensor Details HTML replacement Codes")
 
-    html_replacement_vars = ["{{sysHostName}}",
-                             "{{sysIP}}",
-                             "{{sysDateTime}}",
-                             "{{sysUpTime}}",
-                             "{{sysCPUTemp}}",
-                             "{{sysFreeDisk}}",
-                             "{{sqldb1}}",
-                             "{{sqldb2}}",
-                             "{{SQLEnabled}}",
+    html_replacement_vars = ["{{HostName}}",
+                             "{{IP}}",
+                             "{{DateTime}}",
+                             "{{UpTime}}",
+                             "{{CPUTemp}}",
+                             "{{FreeDisk}}",
+                             "{{IntervalSize}}",
+                             "{{TriggerSize}}",
+                             "{{SQLWriteEnabled}}",
                              "{{CustomEnabled}}"]
 
     return html_replacement_vars
@@ -66,13 +66,13 @@ def html_system_codes():
 def html_config_codes():
     logger.debug("Getting Sensor Config HTML replacement Codes")
 
-    html_replacement_vars = ["{{sysHostName}}",
-                             "{{sysIP}}",
-                             "{{sysDateTime}}",
-                             "{{SQLDataBaseEnable}}",
+    html_replacement_vars = ["{{HostName}}",
+                             "{{IP}}",
+                             "{{DateTime}}",
                              "{{IntervalDuration}}",
                              "{{TriggerDuration}}",
-                             "{{CustomEnable}}",
+                             "{{SQLWriteEnabled}}",
+                             "{{CustomEnabled}}",
                              "{{CustomAcc}}",
                              "{{CustomMag}}",
                              "{{CustomGyro}}"]
@@ -93,111 +93,120 @@ def open_url(url):
     webbrowser.open(url)
 
 
-def sensor_html_report(ip_list, report_type):
+def sensor_system_report(ip_list):
     final_file = ''
     sensor_html = ''
-    replace_word = ''
-    current_sensor_html = ''
     temp_config = load_config()
     net_timeout = int(temp_config.network_details_timeout)
 
-    if report_type == "SystemDetails":
-        replacement_codes = html_system_codes()
-    else:
-        replacement_codes = html_config_codes()
-
     try:
-        if report_type == "SystemDetails":
-            html_file_part = open(str(app_location_directory + html_template_details1), 'r')
-        else:
-            html_file_part = open(str(app_location_directory + html_template_config1), 'r')
+        html_file_part = open(str(app_location_directory + html_template_system1), 'r')
         final_file = html_file_part.read()
         html_file_part.close()
-        if report_type == "SystemDetails":
-            html_file_part = open(str(app_location_directory + html_template_details2), 'r')
-        else:
-            html_file_part = open(str(app_location_directory + html_template_config2), 'r')
+        html_file_part = open(str(app_location_directory + html_template_system2), 'r')
         sensor_html = html_file_part.read()
         html_file_part.close()
-        logger.debug("Open First 2 Templates - OK")
+        logger.debug("Open First 2 System Report Templates - OK")
     except Exception as error:
-        logger.error("Open First 2 Templates - Failed: " + str(error))
+        logger.error("Open First 2 System Report Templates - Failed: " + str(error))
 
     # For each IP in the list, Get its data per Report "Type"
     # Inserting them into a final HTML file, based on a 3 part template
+    replacement_codes = html_system_codes()
     for ip in ip_list:
         try:
             current_sensor_html = sensor_html
+            sensor_data = get_system_info(ip, net_timeout)
 
-            if report_type == "SystemDetails":
-                sensor_data = get_system_info(ip, net_timeout)
-            else:
-                sensor_data = get_sensor_config(ip, net_timeout)
+            uptime_days = int(float(sensor_data[3]) // 1440)
+            uptime_hours = int((float(sensor_data[3]) % 1440) // 60)
+            uptime_min = int(float(sensor_data[3]) % 60)
+            sensor_data[3] = str(uptime_days) + " Days / " + str(uptime_hours) + "." + str(uptime_min) + " Hours"
 
-            count2 = 0
+            count = 0
             for code in replacement_codes:
-                if count2 == 0:
-                    replace_word = str(sensor_data[0])
-                elif count2 == 1:
-                    replace_word = str(sensor_data[1])
-                elif count2 == 2:
-                    replace_word = str(sensor_data[2])
-                elif count2 == 3:
-                    if report_type == "SystemDetails":
-                        uptime_days = int(float(sensor_data[3]) // 1440)
-                        uptime_hours = int((float(sensor_data[3]) % 1440) // 60)
-                        uptime_min = int(float(sensor_data[3]) % 60)
-                        replace_word = str(uptime_days) + " Days / " + str(uptime_hours) + "." + str(uptime_min) + " Hours"
-                    else:
-                        replace_word = str(sensor_data[3])
-                elif count2 == 4:
-                    if report_type == "SystemDetails":
-                        sensor_data[4] = round(float(sensor_data[4]), 2)
-                    replace_word = str(sensor_data[4])
-                elif count2 == 5:
-                    replace_word = str(sensor_data[5])
-                elif count2 == 6:
-                    replace_word = str(sensor_data[6])
-                elif count2 == 7:
-                    replace_word = str(sensor_data[7])
-                elif count2 == 8:
-                    replace_word = str(sensor_data[8])
-                elif count2 == 9:
-                    replace_word = str(sensor_data[9])
-                else:
-                    logger.error("Too many Variables provided - Try Updating the Program")
+                try:
+                    replace_word = str(sensor_data[count])
+                except Exception as error:
+                    replace_word = "Failed"
+                    logger.error("Invalid Sensor Data: " + str(error))
 
+                print(code + " / " + replace_word)
                 current_sensor_html = current_sensor_html.replace(code, replace_word)
-                count2 = count2 + 1
+                count = count + 1
+            final_file = final_file + current_sensor_html
         except Exception as error:
-                logger.error("Report Failure: " + str(error))
+                logger.error("System Report Failure: " + str(error))
 
-        # Add's each sensor that checked Online, into the final HTML variable
-        final_file = final_file + current_sensor_html
     try:
-        if report_type == "SystemDetails":
-            html_file_part = open(str(app_location_directory + html_template_details3), 'r')
-        else:
-            html_file_part = open(str(app_location_directory + html_template_config3), 'r')
-
+        html_file_part = open(str(app_location_directory + html_template_system3), 'r')
         html_end = html_file_part.read()
         html_file_part.close()
         final_file = final_file + html_end
-        logger.debug("Created Sensor Report - HTML File - OK")
+        logger.debug("Created System Report - HTML File - OK")
+    except Exception as error:
+        logger.error("Open 3rd System Report Template File Failed: " + str(error))
+
+    try:
+        save_to_location = str(temp_config.save_to + "SensorsSystem.html")
+        file_out = open(save_to_location, 'w')
+        file_out.write(final_file)
+        file_out.close()
+        open_html(save_to_location)
+        logger.debug("Sensor System Report - HTML Save File - OK")
+    except Exception as error:
+        logger.error("Sensor System Report - HTML Save File - Failed: " + str(error))
+
+
+def sensor_config_report(ip_list):
+    final_file = ''
+    sensor_html = ''
+    temp_config = load_config()
+    net_timeout = int(temp_config.network_details_timeout)
+
+    try:
+        html_file_part = open(str(app_location_directory + html_template_config1), 'r')
+        final_file = html_file_part.read()
+        html_file_part.close()
+        html_file_part = open(str(app_location_directory + html_template_config2), 'r')
+        sensor_html = html_file_part.read()
+        html_file_part.close()
+        logger.debug("Open First 2 Config Report Templates - OK")
+    except Exception as error:
+        logger.error("Open First 2 Config Report Templates - Failed: " + str(error))
+
+    # For each IP in the list, Get its data per Report "Type"
+    # Inserting them into a final HTML file, based on a 3 part template
+    replacement_codes = html_config_codes()
+    for ip in ip_list:
+        try:
+            current_sensor_html = sensor_html
+            sensor_data = get_sensor_config(ip, net_timeout)
+
+            count = 0
+            for code in replacement_codes:
+                current_sensor_html = current_sensor_html.replace(code, str(sensor_data[count]))
+                count = count + 1
+            final_file = final_file + current_sensor_html
+        except Exception as error:
+                logger.error("Config Report Failure: " + str(error))
+
+    try:
+        html_file_part = open(str(app_location_directory + html_template_config3), 'r')
+        html_end = html_file_part.read()
+        html_file_part.close()
+        final_file = final_file + html_end
+        logger.debug("Created Sensor Config Report - HTML File - OK")
     except Exception as error:
         logger.error("Open 3rd Template File Failed: " + str(error))
 
     # Write to a HTML file
     try:
-        if report_type == "SystemDetails":
-            save_to_location = str(temp_config.save_to + "SensorsSystem.html")
-        else:
-            save_to_location = str(temp_config.save_to + "SensorsConfig.html")
-
+        save_to_location = str(temp_config.save_to + "SensorsConfig.html")
         file_out = open(save_to_location, 'w')
         file_out.write(final_file)
         file_out.close()
         open_html(save_to_location)
-        logger.debug("Sensor Report - HTML Save File - OK")
+        logger.debug("Sensor Config Report  - HTML Save File - OK")
     except Exception as error:
-        logger.error("Sensor Report - HTML Save File - Failed: " + str(error))
+        logger.error("Sensor Config Report  - HTML Save File - Failed: " + str(error))
