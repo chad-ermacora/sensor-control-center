f02db3b8284eaa32b60bc76d5f61cc4f34b9d8a8
chad-ermacora <chad.e@linux.com>
2018-12-15 17:41:22 -0800

    Refactored + added License to a few files

--- app_guizero/gui_graphing.py
+++ app_guizero/gui_graphing.py
@@ -16,7 +16,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-
 from tkinter import filedialog
 
 from guizero import Window, CheckBox, PushButton, Text, TextBox, warn, ButtonGroup
--- app_guizero/gui_platform_tweaks.py
+++ app_guizero/gui_platform_tweaks.py
@@ -16,11 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-import os
 import platform
-
-import app_config
-import app_logger
 
 
 def app_custom_configurations(main_app):
