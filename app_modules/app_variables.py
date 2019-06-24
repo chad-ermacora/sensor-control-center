d1a3bcadd8a7a03207480342c66565e5948501b5
chad-ermacora <chad.ermacora@gmail.com>
2019-06-24 10:14:43 -0700

    Updated version

--- app_modules/app_variables.py
+++ app_modules/app_variables.py
@@ -18,7 +18,7 @@
 """
 from sys import path
 
-software_version = "Tested on Python 3.5 & 3.7 || Alpha.25.1"
+software_version = "Tested on Python 3.5 & 3.7 || Alpha.25.2"
 
 
 class CreateSQLColumnsReadable:
