c924023f11fbc123906314d0ef5cbb6ae0daaffb
chad-ermacora <chad.ermacora@gmail.com>
2019-08-02 15:24:25 -0700

    Matching Version to Kootnet Sensors

--- app_modules/app_variables.py
+++ app_modules/app_variables.py
@@ -18,7 +18,7 @@
 """
 from sys import path
 
-software_version = "Tested on Python 3.5 & 3.7 || Alpha.25.223"
+software_version = "Tested on Python 3.5 & 3.7 || Alpha.25.252"
 
 
 class CreateSQLColumnsReadable:
